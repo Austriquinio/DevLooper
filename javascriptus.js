e2399490058c8d525ad947de11172f48ec28af61
LORENZZO AUSTRIQUINIO ACCIOLY <47121326809@info.local>
2024-05-21 17:24:03 -0300

    feat: cadastro com API

--- javascriptus.js
+++ javascriptus.js
@@ -1,35 +1,58 @@
-// let menu = document.querySelector("#menu")
-let menu = document.getElementById("menu")
-let iconeBarras = document.getElementById("icone-barras")
-let iconeX = document.querySelector("#icone-x")
+const burg = document.querySelector(".burg");
+const navMenu = document.querySelector(".nav-menu");
 
-function abrirFecharMenu() {
+burg.addEventListener("click", () => {
+    burg.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
 
-    // Se o menu está fechado
-    if (menu.classList.contains("menu-fechado")) {
-        // Abrir o menu
-        menu.classList.remove("menu-fechado")
+document.querySelectorAll(".nav-link").forEach(n => n. 
+    addEventListener("click", () => {
+        burg.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
 
-        // Mostrar icone X
-        iconeX.style.display = "inline"
 
-        // Esconder icone Barras
-        iconeBarras.style.display = "none"
 
-    } else {
-        // Fechar o menu
-        menu.classList.add("menu-fechado")
-
-        // Esconder icone X
-        iconeX.style.display = "none"
-
-        // Mostrar icone Barras
-        iconeBarras.style.display = "inline"
+    const solicitarOrcamento = (event) => {
+        // Pegar valores dos inputs
+        let valorNome = document.getElementById("firstName").value
+        let valorSobreNome = document.getElementById("lastName").value
+        let valorEmail = document.getElementById("email").value
+        let valorMobile = document.getElementById("mobile").value
+    
+        // Organizar objeto com os valores
+        let dadosForm = {
+            nome: valorNome,
+            nome: valorSobreNome
+            email: valorEmail,
+            descricao: valorDescricao
+        }
+    
+        // Enviar requisicao para a api
+        // 127.0.0.1 = localhost
+        // Método HTTP POST - Create -> Cadastrar ou criar 
+        fetch("http://127.0.0.1:3000/solicitacoes", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dadosForm)
+        })
+        .then(resposta => {
+            console.log(resposta)
+    
+            // Limpar os campos (inputs)
+            document.querySelector("#contato form").reset()
+    
+            // Mostrar alert com msg de sucesso
+            alert("Solicitação cadastrada")
+        })
+        .catch(erro => {
+            // CASO ERRO - alert com msg erro
+            console.error(erro)
+            alert("Erro na requisição")
+        })
+    
+        event.preventDefault()
     }
-}
-
-window.onresize = () => {
-    menu.classList.remove("menu-fechado")
-    iconeX.style.display = "inline"
-    iconeBarras.style.display = "none"
-}
